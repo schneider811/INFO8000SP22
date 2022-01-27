6c6992ac51c387780ad7ad971ff7367d79ae4ad8
Keith Schneider <Keith.schneider811@gmail.com>
2022-01-27 12:09:55 -0500

    hi

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from tracemalloc import stop
+
+
+print("Loading the file")
+f = open("test.txt")
+lines = f.readlines()
+print(lines)
+
+the_sum = 0
+
+for line in lines:
+    the_sum = the_sum + int(line) 
+    ##print(the_sum)
+
+print(sum([int(x) for x in lines]))
