bfa546252eef9a60d5570399c5cc9cee31dcc6b5
Keith Schneider <Keith.schneider811@gmail.com>
2022-02-01 11:41:00 -0500

    started list access

--- Test.py
+++ Test.py
@@ -3,13 +3,20 @@ from tracemalloc import stop
 
 print("Loading the file")
 f = open("test.txt")
+print(f)
 lines = f.readlines()
 print(lines)
+lines = [x.strip() for x in lines]#convert to string
+print(lines)
+
 
 the_sum = 0
 
 for line in lines:
-    the_sum = the_sum + int(line) 
-    ##print(the_sum)
+    the_sum = the_sum + int(line.strip()) 
+print(f"The sum is {the_sum}")
 
-print(sum([int(x) for x in lines]))
+#print("The sum is",the_sum)
+
+print("The sum is also",sum([int(x) for x in lines])) #prints the sum of the numbers in lines
+
---
+++ lists.py
@@ -0,0 +1,2 @@
+a_list = [34,13,95]
+print(a_list[0])
